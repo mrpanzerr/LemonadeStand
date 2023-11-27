8da6b29bca9a457b1f4cd2b9dfb2878eee7065f2
mrpanzerr <147166488+mrpanzerr@users.noreply.github.com>
2023-11-26 22:54:00 -0500

    commented NormalLemonade.java

--- NormalLemonade.java
+++ NormalLemonade.java
@@ -1,16 +1,21 @@
 public class NormalLemonade extends Drink {
 
+    // Constructor for NormalLemonade, takes size as a parameter
     public NormalLemonade(String size) {
+        // Call the constructor of the superclass (Drink) with default flavor ("Lemonade"), size, and price
         super("Lemonade", size, 2.99);
-        if(this.getSize().equals("Large")){
+
+        // Adjust inventory based on the size of the lemonade being created
+        if (this.getSize().equals("Large")) {
+            // Subtract 3 units of sugar and 3 lemons for a Large lemonade
             Inventory.subtractSugar(3);
             Inventory.subtractLemons(3);
-        }
-        else if(this.getSize().equals("Small")){
+        } else if (this.getSize().equals("Small")) {
+            // Subtract 1 unit of sugar and 1 lemon for a Small lemonade
             Inventory.subtractLemons(1);
             Inventory.subtractSugar(1);
-        }
-        else{
+        } else {
+            // Subtract 2 units of sugar and 2 lemons for a Medium (default) lemonade
             Inventory.subtractSugar(2);
             Inventory.subtractLemons(2);
         }
